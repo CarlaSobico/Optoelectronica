0317f5d035fa6f2ceb058d3516e7f0103858e853
ascopa <64786223+ascopa@users.noreply.github.com>
2020-05-06 21:13:14 -0300

    Update ajuste_x.m
    Correción del grafico con color, se le agregaron ejes

--- Trabajo practico 1/Codigos/ajuste_x.m
+++ Trabajo practico 1/Codigos/ajuste_x.m
@@ -11,7 +11,7 @@ axis on;
 
 %busco por filas las que estan por arriba de un umbral y les calculo el ancho, luego busco la mas ancha
 for i = 1:rows_img01
-    x_umbrales01 = find(img01(i,:) > 0.7);%%Valor para tocar N1
+    x_umbrales01 = find(img01(i,:) > 0.5);%%Valor para tocar N1
     if (length(x_umbrales01) >= 0.7) %%Valor para tocar N2
         ancho01(i,1) = x_umbrales01(end) - x_umbrales01(1);
     else
@@ -20,20 +20,21 @@ for i = 1:rows_img01
 end
 [~, y_max_ancho01] = max(ancho01);
 
-figure(2);
-hold on;
+Hf = figure(2);
+set(Hf,'PaperPosition',[0 0 30 8]); %Defino el tama?o del grafico que va
 imshow(gray2ind(img01), jet());
-hold on;
-plot(1:columns_img01, y_max_ancho01*ones(1,columns_img01), '-k', 'linewidth', 2);
+line(1:columns_img01, y_max_ancho01*ones(1,columns_img01), 'LineWidth', 2, 'Color', 'k');
+%edito los ejes
 axis on;
-xlabel('x / px');
-ylabel('y / px');
+set(gca(),'XLim',[1 columns_img01],'YLim',[1 rows_img01],'Box','on','FontName','Arial','FontSize',8,'GridLineStyle','--','LineWidth',0.4,'TickDir','in');
+xlabel('x / px','FontName','Arial','FontSize',20);
+ylabel('y / px','FontName','Arial','FontSize',20)
 
 med_max_x01 = img01(y_max_ancho01, :);
 x01 = 0:(columns_img01-1);
 
 %busco el valor central de la medicion y corro el eje x
-x_umbr01 = find(med_max_x01 > 0.5);%%Valor para tocar N3
+x_umbr01 = find(med_max_x01 > 0.25);%%Valor para tocar N3
 x_cent01 = floor((x_umbr01(1) + x_umbr01(end))/2);
 x01 = x01 - x_cent01;
 
