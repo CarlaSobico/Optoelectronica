db62d5d9c630f3543f229fb19dd8edade2e022d5
ascopa <64786223+ascopa@users.noreply.github.com>
2020-05-09 17:05:53 -0300

    ajuste rayos

--- Trabajo practico 1/Codigos/TP1.m
+++ Trabajo practico 1/Codigos/TP1.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 %Focos primer setup
-f4_a=30*1e-3;
+f4_a=-30*1e-3;
 f2_a=200*1e-3;
 %Foco segundo setup
 f4_b=200*1e-3;
@@ -10,6 +10,10 @@ f2_b=-30*1e-3;
 Ye=0.001;
 Ye2=0.003;
 Oe=0;
+L0=0.05; %lente inicial
+d=(f2_a/f4_a +1)*f4_a; %dist entre lents
+h0=0; %altura graf
+h1=25;
 
 V1=PuntosRayo(f2_a,f4_a,Ye,Oe);
 V2=PuntosRayo(f2_b,f4_b,Ye,Oe);
@@ -23,24 +27,16 @@ hold on
 plot(x,V2*1e3,'Color',[0, 1, 0],'LineWidth',2);
 plot(x,V3*1e3,'Color',[0, 0, 1],'LineWidth',2);
 plot(x,V4*1e3,'Color',[1, 0, 1],'LineWidth',2);
-
-%edito los ejes
+plot([L0 L0],[h0 h1],'--k');
+plot([L0+d L0+d],[h0 h1],'-.k');
 set(gca(),'Box','on','FontName','Arial','FontSize',8,'GridLineStyle','--','LineWidth',0.4,'TickDir','in');
-%yticks([-15:1:15])
-%xticks([-15:.05:20])
-%Los XTick, YTick son para marcar numeros en el grafico, se pueden poner
-%los q queres (como hice en YTick) o el intervalo con el step (como hice en
-%Xtick)
-
 xlabel('Distancia recorrida [m]','FontName','Arial','FontSize',20); %etiquetas de los ejes
 ylabel('Ancho rayo [mm]','FontName','Arial','FontSize',20);
-
-grid on; %lineas intermedias
+grid on; 
 hold on;
 
-Hleg=legend('H=1mm - Foco_{L1}:200mm - Foco_{L2}:-30mm','H=1mm - Foco_{L1}:-30mm - Foco_{L2}:200mm','H=3mm - Foco_{L1}:200mm - Foco_{L2}:-30mm','H=3mm - Foco_{L1}:-30mm - Foco_{L2}:200mm'); %Leyenda. Fijense de verificar que 
-%queden bien referenciadas o cambien el orden!!!
+Hleg=legend('H=1mm - Foco_{L1}:200mm - Foco_{L2}:-30mm','H=1mm - Foco_{L1}:-30mm - Foco_{L2}:200mm','H=3mm - Foco_{L1}:200mm - Foco_{L2}:-30mm','H=3mm - Foco_{L1}:-30mm - Foco_{L2}:200mm','Lente 1','Lente 2'); %Leyenda. Fijense de verificar que 
 legend('boxon');
-set(Hleg,'location','northwest','FontName','Arial','FontSize',10);
+set(Hleg,'location','northwest','FontName','Arial','FontSize',7.5);
 
-%print('schmitTrigInvTranfVar.png','-dpng');
+print('caminoRayos.png','-dpng');
